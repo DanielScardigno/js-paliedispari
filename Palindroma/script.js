aa4f1bfc1f90838a20db435bd92f997796ddd718
DanielScardigno <danielscardigno2@gmail.com>
2025-02-25 22:29:14 +0100

    finished first exercise

---
+++ Palindroma/script.js
@@ -0,0 +1,27 @@
+//input
+const userWord = prompt("Scrivi una Parola");
+
+//output
+let messaggio;
+if (userWord.length === 0) {
+    messaggio = "Errore, inserisci una parola"
+} else if (ELaParolaPalindroma(userWord) === true) {
+    messaggio = `${userWord} è una parola palindroma!`
+} else {
+    messaggio = `${userWord} non è una parola palindroma`
+}
+console.log(messaggio);
+
+//Funzione
+function ELaParolaPalindroma(word) {
+    const letters = [];
+    for (let i = 0; i < userWord.length; i++) {
+        letters.push(word[i]);
+    }
+    const reverseWord = letters.reverse();
+    if (reverseWord.join("") === userWord) {
+        return true;
+    } else {
+        return false;
+    }
+}
